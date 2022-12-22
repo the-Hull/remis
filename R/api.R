9b34429bdee50a0fe43f5791f93d1dbd41e516ce
the-Hull <the-Hull@users.noreply.github.com>
2022-12-22 20:41:35 +0100

    package structure setup

---
+++ R/api.R
@@ -0,0 +1,39 @@
+# api meta ----------------------------------------------------------------
+
+
+
+meta <- list(
+  api_base_url = "https://di.unfccc.int/",
+  # choose annex/parties/groups
+  api_parties = "api/parties/",
+  # choose years
+  api_years = "api/years/single",
+  # get units
+  api_unit = "conversion/fq",
+  # variables for query here
+  api_inst_categories = "api/dimension-instances/category",
+  api_inst_classification = "api/dimension-instances/classification",
+  api_inst_measures = "api/dimension-instances/measure",
+  api_inst_gas = "api/dimension-instances/gas",
+  # variable collection?
+  api_variables = "api/variables/fq/"
+)
+
+
+
+# rem_init ----------------------------------------------------------------
+
+rem_init <- function(){
+
+  # category
+
+  # classification
+
+  # measure
+
+  # gas
+
+  # unit
+
+
+}
